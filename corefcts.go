8a5b2c5875c8e73ec0034f3b7b364a0c6af1ede9
fabdem <demurger@gmail.com>
2020-10-01 14:21:38 -0700

    use regex
    use regex to parse the change list description

--- corefcts.go
+++ corefcts.go
@@ -5,11 +5,11 @@ package perforce
 import (
 	"errors"
 	"fmt"
-	//"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -23,40 +23,40 @@ import (
 //		- the file in a temp file in os.TempDir()
 //		- its 'perfore name' with revision number for info. This is not the temp file name
 //		- err code, nil if okay
-func (p *Perforce) GetFile(depotFile string, rev int) ( tempFile *os.File ,fileName string, err error) {
+func (p *Perforce) GetFile(depotFile string, rev int) (tempFile *os.File, fileName string, err error) {
 	p.log("GetFile()\n")
 
 	fileName = filepath.Base(depotFile) // extract filename
-	ext := filepath.Ext(depotFile)       // Read extension
+	ext := filepath.Ext(depotFile)      // Read extension
 
 	if rev > 0 { // If a specific version is requested
 		fileName = fileName[0:len(fileName)-len(ext)] + "#" + strconv.Itoa(rev) + ext
-	} else {    // Get head rev
+	} else { // Get head rev
 		rev, err = p.GetHeadRev(depotFile)
 		if err != nil {
-			return tempFile,fileName, err
+			return tempFile, fileName, err
 		}
 		fileName = fileName[0:len(fileName)-len(ext)] + "#" + strconv.Itoa(rev) + ext
 	} // fileName is provided as a convenience
 
-	p.log(fmt.Sprintf("fileName=%s rev=%d\n",fileName, rev))
+	p.log(fmt.Sprintf("fileName=%s rev=%d\n", fileName, rev))
 
 	tempFile, err = ioutil.TempFile("", "perforce_getfile_")
 	if err != nil {
-			return tempFile,fileName, errors.New(fmt.Sprintf("Unable to create a temp file - %v", err))
+		return tempFile, fileName, errors.New(fmt.Sprintf("Unable to create a temp file - %v", err))
 	}
 	defer tempFile.Close()
 
 	var out []byte
 
 	if len(p.user) > 0 {
-		out, err = exec.Command(p.p4Cmd, "-u", p.user, "print", "-k", "-q", "-o", tempFile.Name(), depotFile + "#" + strconv.Itoa(rev) ).CombinedOutput()
+		out, err = exec.Command(p.p4Cmd, "-u", p.user, "print", "-k", "-q", "-o", tempFile.Name(), depotFile+"#"+strconv.Itoa(rev)).CombinedOutput()
 		// fmt.Printf("P4 command line result - %s\n %s\n", err, out)
 	} else {
-		out, err = exec.Command(p.p4Cmd, "print", "-k", "-q", "-o", tempFile.Name(), depotFile + "#" + strconv.Itoa(rev)).CombinedOutput()
+		out, err = exec.Command(p.p4Cmd, "print", "-k", "-q", "-o", tempFile.Name(), depotFile+"#"+strconv.Itoa(rev)).CombinedOutput()
 	}
 	if err != nil {
-		return tempFile,fileName, errors.New(fmt.Sprintf("p4 command line error %s - %s ",err, out))
+		return tempFile, fileName, errors.New(fmt.Sprintf("p4 command line error %s - %s ", err, out))
 	}
 
 	// 2EME PROBLEME POURQUOI CA PANIQUE SI ERROR DS GETFILE (REMOVE USER)
@@ -66,12 +66,12 @@ func (p *Perforce) GetFile(depotFile string, rev int) ( tempFile *os.File ,fileN
 	// So manually checking if a file was created:
 	if _, err = os.Stat(tempFile.Name()); err != nil {
 		if os.IsNotExist(err) { // file does not exist
-			return tempFile,fileName, errors.New(fmt.Sprintf("P4 no file created %v - %v ",out, err))
+			return tempFile, fileName, errors.New(fmt.Sprintf("P4 no file created %v - %v ", out, err))
 		} else { // Can't get file stat
-			return tempFile,fileName, errors.New(fmt.Sprintf("Can't access the status of file produced %v - %v ",out, err))
+			return tempFile, fileName, errors.New(fmt.Sprintf("Can't access the status of file produced %v - %v ", out, err))
 		}
 	}
-	return tempFile,fileName, nil  // everything is fine returns file and file name
+	return tempFile, fileName, nil // everything is fine returns file and file name
 }
 
 // GetHeadRev()
@@ -122,7 +122,7 @@ func (p *Perforce) GetHeadRev(depotFileName string) (rev int, err error) {
 //		- Change List number
 //  Return:
 //		- a map of files (depot path and name) and rev numbers
-//		- CL's user and workspace
+//		- CL's user and workspace for sanity check
 //		- err code, nil if okay
 /*
 p4 -uxxxx describe 6102201
@@ -138,7 +138,7 @@ Affected files ...
 
 */
 
-func (p *Perforce) GetPendingCLContent(changeList int) ( m_files map[string]int, user string, workspace string, err error) {
+func (p *Perforce) GetPendingCLContent(changeList int) (m_files map[string]int, user string, workspace string, err error) {
 	p.log("GetChangeListContent()\n")
 
 	var out []byte
@@ -154,62 +154,86 @@ func (p *Perforce) GetPendingCLContent(changeList int) ( m_files map[string]int,
 		// out, err := exec.Command(p.p4Cmd, "info").Output()
 	}
 	if err != nil {
-		return m_files,"","", errors.New(fmt.Sprintf("P4 command line error %v  out=%s", err, out))
+		return m_files, "", "", errors.New(fmt.Sprintf("P4 command line error %v  out=%s", err, out))
 	}
 
-	// Get CL user and workspace - TBD replace with a regex search
-	cue1 := "Change " + strconv.Itoa(changeList) + " by "
+	// Parse result file
+	cue1 := "Change "
+	cue2 := " by "
+	cue3 := "@"
+	cue4 := " on "
+	cue5 := "Affected files ..."
+	cue6 := "... "
+	cue7 := "#"
+	cue8 := " edit"
 
-	idx := strings.Index(string(out), cue1)
+	// Get sCL
+	r, _ := regexp.Compile(cue1 + `\d+` + cue2)
+	sCL := r.FindString(string(out))
+	sCL = strings.TrimPrefix(sCL, cue1)
+	sCL = strings.TrimSuffix(sCL, cue2)
+	cl, _ := strconv.Atoi(sCL)
+
+	// Get User
+	r, _ = regexp.Compile(cue2 + `[^` + cue3 + `]+` + cue3)
+	sUSER := r.FindString(string(out))
+	sUSER = strings.TrimPrefix(sUSER, cue2)
+	sUSER = strings.TrimSuffix(sUSER, cue3)
+
+	// Get workspace
+	r, _ = regexp.Compile(cue3 + `[^ ]+` + cue4)
+	sWS := r.FindString(string(out))
+	sWS = strings.TrimPrefix(sWS, cue3)
+	sWS = strings.TrimSuffix(sWS, cue4)
+
+	// Add a check on the values above since they are mandatory
+	if sCL == "" || sUSER == "" || sWS == "" || cl != changeList {
+		return m_files, "", "", errors.New(fmt.Sprintf("Error parsing P4 response - missing or incorrect  field(s) %s %s %s in out=%s", sCL, sUSER, sWS, out))
+		return
+	}
+
+	// Move start at the beginning of the file list
+	idx := strings.Index(string(out), cue5)
 	if idx == -1 {
-		return m_files,"","", errors.New(fmt.Sprintf("Error parsing P4 response - missing user and workspace %s in out=%s", cue1, out))
+		return m_files, "", "", errors.New(fmt.Sprintf("Error parsing P4 response - missing field %s in out=%s", cue5, out))
 	}
-
-	endFieldIdx := strings.Index(string(out[idx + len(cue1):])," ") + len(cue1)
+	idx += strings.Index(string(out[idx:]), cue6)
 	if idx == -1 {
-		return m_files,"","", errors.New(fmt.Sprintf("Error parsing P4 response - malformed responsein out=%s", out))
+		return m_files, "", "", errors.New(fmt.Sprintf("Error parsing P4 response - missing field %s in out=%s", cue6, out))
 	}
 
-	field := string(out[idx + len(cue1): endFieldIdx])
-	usrAndWs := strings.Split(field, "@")
-	if len(usrAndWs) != 2 {
-		return m_files,"","", errors.New(fmt.Sprintf("Error parsing P4 response - issue getting user and workspace %s in out=%s", field, out))
-	}
-	user = usrAndWs[0]
-	workspace = usrAndWs[1]
+	// Prep regexs
+	r_file, _ := regexp.Compile(cue6 + `//[^` + cue7 + `]+` + cue7)
+	r_rev, _ := regexp.Compile(cue7 + `\d+` + cue8)
 
-	// Get list of affected files
-	cue2 := "Affected files ..."
-	cue3 := "... //"
-	cue4 := "... "
-	cue5 := " edit"
-	idx = strings.Index(string(out), cue2)
-	if idx == -1 {
-		return m_files,"","", errors.New(fmt.Sprintf("Error parsing P4 response - missing field %s in out=%s", cue2, out))
-	}
-	idx = strings.Index(string(out), cue3)
-	if idx == -1 {
-		return m_files,"","", errors.New(fmt.Sprintf("Error parsing P4 response - missing field %s in out=%s", cue3, out))
-	}
+	lines := strings.Split(string(out[idx:]), "\n")
 
-	lines := strings.Split(string(out[idx:]),"\n")
+	// Get files
 	for _, line := range lines {
 		// fmt.Printf("line %s\n", line)
-		if strings.Index(line, cue3) == -1 { // If there is no "... //" we're done
+		sFILE := r_file.FindString(line)
+		sFILE = strings.TrimPrefix(sFILE, cue6)
+		sFILE = strings.TrimSuffix(sFILE, cue7)
+		// fmt.Printf("FILE=%s\n",sFILE)
+
+		sREV := r_rev.FindString(line)
+		sREV = strings.TrimPrefix(sREV, cue7)
+		sREV = strings.TrimSuffix(sREV, cue8)
+		// fmt.Printf("REV=%s\n",sREV)
+
+		if sFILE == "" || sREV == "" { // If empty we're done
 			break
 		}
-		line = strings.TrimPrefix(line, cue4) // Isolate path
-		line = strings.TrimSuffix(line, cue5)
-		fileAndVersion := strings.Split(line,"#")
-		file := fileAndVersion[0]
-		version,err :=  strconv.Atoi(fileAndVersion[1])
-		if err !=nil || len(file) <= 0 || version <= 0 {
-			return m_files,"","", errors.New(fmt.Sprintf("Error parsing P4 response - file details %s in out=%s", line, out))
+
+		version, err := strconv.Atoi(sREV)
+		if err != nil {
+			return m_files, "", "", errors.New(fmt.Sprintf("Error parsing P4 response - incorrect revision number in line=%s,  err=%s", line, err))
 		}
-		m_files[file] = version // populate the map
+
+		m_files[sFILE] = version // populate the map
 	}
 
-	return m_files, user, workspace, nil
+	return m_files, sUSER, sWS, nil
 }
 
 // DiffHeadnWorkspace()
@@ -230,29 +254,29 @@ add 3 chunks 8 lines
 deleted 2 chunks 7 lines
 changed 1 chunks 1 / 2 lines
 */
-func (p *Perforce) DiffHeadnWorkspace(aFileInDepot string) ( diffedFileInDepot string,diffedFileInWorkspace string, addedLines int,removedLines int,changedLines int, err error) {
+func (p *Perforce) DiffHeadnWorkspace(aFileInDepot string) (diffedFileInDepot string, diffedFileInWorkspace string, addedLines int, removedLines int, changedLines int, err error) {
 	p.log("DiffHeadnWorkspace()\n")
 
 	var out []byte
-	option := "-ds"  // Summary output
+	option := "-ds" // Summary output
 	if p.diffignorespace {
 		option += "b" // plus changes within spaces will be ignored
 	}
 
 	if len(p.workspace) <= 0 {
-		return "","",0,0,0,errors.New(fmt.Sprintf("P4 command line error - a workspace needs to be defined"))
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("P4 command line error - a workspace needs to be defined"))
 	}
 	if len(p.user) > 0 {
 		// fmt.Printf(p4Cmd + " -u " + user + " -c " + workspace + " diff -ds " + " " + aFileInDepot + "\n")
-		out, err = exec.Command(p.p4Cmd, "-u", p.user,"-c", p.workspace, "diff",option, aFileInDepot).CombinedOutput()
+		out, err = exec.Command(p.p4Cmd, "-u", p.user, "-c", p.workspace, "diff", option, aFileInDepot).CombinedOutput()
 		//fmt.Printf("P4 command line result - err=%s\n out=%s\n", err, out)
 	} else {
 		// fmt.Printf(p4Cmd + " -c " + workspace + " diff -ds " + " " + aFileInDepot + "\n")
-		out, err = exec.Command(p.p4Cmd,"-c", p.workspace, "diff","option", aFileInDepot).CombinedOutput()
+		out, err = exec.Command(p.p4Cmd, "-c", p.workspace, "diff", "option", aFileInDepot).CombinedOutput()
 		// out, err := exec.Command(p.p4Cmd, "info").Output()
 	}
 	if err != nil {
-		return "","",0,0,0, errors.New(fmt.Sprintf("P4 command line error %v  out=%s", err, out))
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("P4 command line error %v  out=%s", err, out))
 	}
 
 	// Parse result
@@ -260,29 +284,29 @@ func (p *Perforce) DiffHeadnWorkspace(aFileInDepot string) ( diffedFileInDepot s
 	cue2 := "==== "
 	cue3 := " ===="
 	cue4 := " - "
-	fields := strings.Split(string(out),cue1)
+	fields := strings.Split(string(out), cue1)
 	if len(fields) < 1 {
-		return "","",0,0,0, errors.New(fmt.Sprintf("P4 command line - parsing error  out=%s", out))
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("P4 command line - parsing error  out=%s", out))
 	}
-	line := fields[1] // 1st line is supposed to contain path of files in depot and workspace.
+	line := fields[1]                     // 1st line is supposed to contain path of files in depot and workspace.
 	line = strings.TrimPrefix(line, cue2) // Isolate paths
 	line = strings.TrimSuffix(line, cue3)
 	fields = strings.Split(line, cue4)
 	if len(fields) < 2 {
-		return "","",0,0,0, errors.New(fmt.Sprintf("P4 command line - parsing error in %s\n out=%s",line, out))
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("P4 command line - parsing error in %s\n out=%s", line, out))
 	}
 	diffedFileInDepot = fields[0]
 	diffedFileInWorkspace = fields[1]
 
-	fields = strings.Split(string(out),cue3)  // Split to get section with line stats
+	fields = strings.Split(string(out), cue3) // Split to get section with line stats
 	if len(fields) < 2 {
-		return "","",0,0,0, errors.New(fmt.Sprintf("1 - P4 command line - parsing error in out=%s\n",out))
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("1 - P4 command line - parsing error in out=%s\n", out))
 	}
 	// fmt.Printf("\n\n\nfields[1]\n%s\n\n",fields[1])
 
-	lines := strings.Split(fields[1],"\n") // Get the section with line stats
+	lines := strings.Split(fields[1], "\n") // Get the section with line stats
 	if len(lines) < 4 {
-		return "","",0,0,0, errors.New(fmt.Sprintf("2 - P4 command line - parsing error in %s\n out=%s\n",lines,out))
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("2 - P4 command line - parsing error in %s\n out=%s\n", lines, out))
 	}
 	// fmt.Printf("\n\nlines[]\n%s\n%s\n%s\n",lines[1],lines[2],lines[3])
 
@@ -291,26 +315,26 @@ func (p *Perforce) DiffHeadnWorkspace(aFileInDepot string) ( diffedFileInDepot s
 		deleted 2 chunks 7 lines
 		changed 1 chunks 1 / 2 lines
 	*/
-	if (strings.Index(lines[1],"add") == -1)||(strings.Index(lines[2],"deleted") == -1)||(strings.Index(lines[3],"changed") == -1) {
-		return "","",0,0,0, errors.New(fmt.Sprintf("3 - P4 command line - parsing error in:\n%s\n%s\n%s\n out=%s\n",lines,out))
+	if (strings.Index(lines[1], "add") == -1) || (strings.Index(lines[2], "deleted") == -1) || (strings.Index(lines[3], "changed") == -1) {
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("3 - P4 command line - parsing error in:\n%s\n%s\n%s\n out=%s\n", lines, out))
 	}
 	addLine := strings.Fields(lines[1])
 	removeLine := strings.Fields(lines[2])
 	changeLine := strings.Fields(lines[3])
 
 	//fmt.Printf("addLine:%v\nremoveLine:%v\nchangeLine:%v\n",addLine,removeLine,changeLine)
-	if (len(addLine) < 4)||(len(removeLine) < 4)||(len(changeLine) < 5) {
-		return "","",0,0,0, errors.New(fmt.Sprintf("4 - P4 command line - parsing error out=%s\n",out))
+	if (len(addLine) < 4) || (len(removeLine) < 4) || (len(changeLine) < 5) {
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("4 - P4 command line - parsing error out=%s\n", out))
 	}
 	var err1, err2, err3, err4 error
-	addedLines,err1 = strconv.Atoi(addLine[3])
-	removedLines,err2 = strconv.Atoi(removeLine[3])
-	changedLines1,err3 := strconv.Atoi(changeLine[3])
-	changedLines2,err4 := strconv.Atoi(changeLine[5])
-	changedLines = changedLines1 + changedLines2  // Not too clear how p4 compute this - see https://community.perforce.com/s/article/10639
-	if (err1 != nil)||(err2 != nil)||(err3 != nil)||(err4 != nil) {
-		return "","",0,0,0, errors.New(fmt.Sprintf("5 - P4 command line - parsing error out=%s\n",out))
+	addedLines, err1 = strconv.Atoi(addLine[3])
+	removedLines, err2 = strconv.Atoi(removeLine[3])
+	changedLines1, err3 := strconv.Atoi(changeLine[3])
+	changedLines2, err4 := strconv.Atoi(changeLine[5])
+	changedLines = changedLines1 + changedLines2 // Not too clear how p4 compute this - see https://community.perforce.com/s/article/10639
+	if (err1 != nil) || (err2 != nil) || (err3 != nil) || (err4 != nil) {
+		return "", "", 0, 0, 0, errors.New(fmt.Sprintf("5 - P4 command line - parsing error out=%s\n", out))
 	}
 
-	return diffedFileInDepot,diffedFileInWorkspace,addedLines,removedLines,changedLines,nil
+	return diffedFileInDepot, diffedFileInWorkspace, addedLines, removedLines, changedLines, nil
 }
--- perforce.go
+++ perforce.go
@@ -5,37 +5,30 @@ package perforce
 //					p4 installed and in path
 //
 //
-//   no timeouts
+//   Limitation: timeouts not managed (relies on p4 cli implementation)
 
 // New()                create an instance/workspace
-
-//  Local paths are systematically enclosed with double quotes in ws def but not in def files.
 
 
 import (
 	"errors"
 	"fmt"
 	"io"
-	//"io/ioutil"
- 	"log"
-	//"os"
+	"log"
 	"os/exec"
 	"time"
-	//"path/filepath"
-	//"strconv"
-	//"strings"
 )
 
 type Perforce struct {
-	user						string 			// optional p4 user
-	workspace				string 			// optional p4 workspace (required by some functions)
-	p4Cmd 					string 			// p4 command and path
-	logWriter   		io.Writer
-	debug						bool
-	diffignorespace	bool				// when set diff ignore spaces and eol
+	user            string // optional p4 user
+	workspace       string // optional p4 workspace (required by some functions)
+	p4Cmd           string // p4 command and path
+	logWriter       io.Writer
+	debug           bool
+	diffignorespace bool // when set diff ignore spaces and eol
 }
 
-// Create an instance
+// Create a new instance
 // - lookup path to p4 command
 // - Returns instance and error code
 func New(user string, workspace string) (*Perforce, error) {
@@ -49,7 +42,7 @@ func New(user string, workspace string) (*Perforce, error) {
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("Unable to find path to p4 command - %v", err))
 	}
-	p.debug = false  // default
+	p.debug = false // default
 	return p, nil
 }
 
@@ -64,13 +57,13 @@ func (p *Perforce) SetWorkspace(workspace string) {
 }
 
 // GetUser()
-func (p *Perforce) GetUser()(user string) {
-	return(p.user)
+func (p *Perforce) GetUser() (user string) {
+	return (p.user)
 }
 
 // GetWorkspace()
-func (p *Perforce) GetWorkspace()(workspace string) {
-	return(p.workspace)
+func (p *Perforce) GetWorkspace() (workspace string) {
+	return (p.workspace)
 }
 
 // SetDiffIgnoreSpace()
@@ -84,8 +77,8 @@ func (p *Perforce) ResetDiffIgnoreSpace() {
 }
 
 // GetDiffIgnoreSpace()
-func (p *Perforce) GetDiffIgnoreSpace()(flag bool) {
-	return(p.diffignorespace)
+func (p *Perforce) GetDiffIgnoreSpace() (flag bool) {
+	return (p.diffignorespace)
 }
 
 // ---------------------------------------
